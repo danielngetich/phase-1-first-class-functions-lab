a9d9a7281179aa3ff51528405a9fb52a999505e9
danielngetich <daniel.ngetich@student.moringaschool.com>
2022-08-21 12:49:11 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers=function(drivers){
+    return (drivers.slice(0,2))
+}
+const returnLastTwoDrivers=function(drivers){
+    return (drivers.slice(2,4))
+}
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(){
+    return (function fareMultiplier(n){
+        return (n*n)
+    })
+}
+function fareDoubler(n){
+return (function fareMultiplier(){
+          return(n*2)
+}())
+}
+function fareTripler(n){
+    return (function fareMultiplier(){
+              return(n*3)
+    }())
+    }
+
+
+function selectDifferentDrivers(drivers,fnn){
+    if (fnn===returnFirstTwoDrivers){
+        return (drivers.slice(0,2));
+    } else if(fnn=returnLastTwoDrivers){
+        return(drivers.slice(2,4));
+    }
+
+}
